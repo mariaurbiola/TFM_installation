cef52dc54f1cb819c0abc5dad09904aaddaa1ed9
mariaurbiola <99193644+mariaurbiola@users.noreply.github.com>
2023-06-14 11:55:22 +0200

    imageFileDetectionCHanged

--- imageFileDetection.py
+++ imageFileDetection.py
@@ -12,8 +12,8 @@ def runImageFileDetection(file_path):
         
     #path_to_image_folder = os.path.dirname(__file__) + '/mmpose_photos/'
 
-    config_file = os.path.dirname(__file__) + '/mmpose/associative_embedding_hrnet_w32_coco_512x512.py'
-    checkpoint_file = os.path.dirname(__file__) + '/mmpose/hrnet_w32_coco_512x512-bcb8c247_20200816.pth'
+    config_file = os.path.dirname(__file__) + '/files_detection/associative_embedding_hrnet_w32_coco_512x512.py'
+    checkpoint_file = os.path.dirname(__file__) + '/files_detection/hrnet_w32_coco_512x512-bcb8c247_20200816.pth'
     pose_model = init_pose_model(config_file, checkpoint_file, device='cpu')  # or device='cuda:0'
 
     #image_name = file_path + 'persons.jpg'
